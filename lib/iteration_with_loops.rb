2c13adc916a0799c554223acc0d89a37ec3e443e
Alec Jasik <alec_jasik@yahoo.com>
2020-08-09 16:58:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,8 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do 
-    if 
-   outer_results << src[row_index][element_index].min
+    if minimum_element < 
+   outer_results << minimum_element
    element_index_index += 1
   end
   row_index += 1
